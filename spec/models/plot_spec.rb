172026cdbf235d0abf5490b1693324e4babb590c
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-11 18:16:58 -0600

    Add test for Plot#available_text

--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -7,9 +7,52 @@ RSpec.describe Plot do
 
   describe 'instance methods' do
     it 'exists' do
-      plot = Plot.new()
+      garden = Garden.create!( name:"North Boulder Community Garden",
+                                      water_on: false,
+                                      water_access_pts: 2,
+                                      created_at: Time.now,
+                                      updated_at: Time.now
+                                    )
+      plot = garden.plots.create!(  name: "The Grove",
+                            available: true,
+                            sun_coverage: :full_shade,
+                            square_ft: 250
+                          )
 
       expect(plot).is_a? Plot
+    end
+
+    describe '#available_text' do
+      it 'returns Yes if plot is available' do
+        garden = Garden.create!( name:"North Boulder Community Garden",
+                                        water_on: false,
+                                        water_access_pts: 2,
+                                        created_at: Time.now,
+                                        updated_at: Time.now
+                                      )
+        plot = garden.plots.create!(  name: "The Grove",
+                              available: true,
+                              sun_coverage: :full_shade,
+                              square_ft: 250
+                            )
+
+      expect(plot.available_text).to eq "Yes"
+      end
+
+      it 'returns No if plot is not available' do
+        garden = Garden.create!( name:"North Boulder Community Garden",
+                                        water_on: false,
+                                        water_access_pts: 2,
+                                        created_at: Time.now,
+                                        updated_at: Time.now
+                                      )
+        plot = garden.plots.create!(  name: "The Grove",
+                              available: false,
+                              sun_coverage: :full_shade,
+                              square_ft: 250
+                            )
+        expect(plot.available_text).to eq "No"
+      end
     end
   end
 end
