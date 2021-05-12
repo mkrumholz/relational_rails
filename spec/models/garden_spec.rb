8caaa87227991f0b3adee5fad21ed5f235357dcd
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-11 18:21:58 -0600

    Update Gardens#show view and create test for #water_on_text

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -7,9 +7,36 @@ RSpec.describe Garden do
 
   describe 'instance methods' do
     it 'exists' do
-      garden = Garden.new()
+      garden = Garden.create!( name:"North Boulder Community Garden",
+                                      water_on: false,
+                                      water_access_pts: 2,
+                                      created_at: Time.now,
+                                      updated_at: Time.now
+                                    )
 
       expect(garden).is_a? Garden
+    end
+
+    describe '#water_on_text' do
+      it 'returns Yes if water is on' do
+        garden = Garden.create!( name:"North Boulder Community Garden",
+                                        water_on: true,
+                                        water_access_pts: 2,
+                                        created_at: Time.now,
+                                        updated_at: Time.now
+                                      )
+        expect(garden.water_on_text).to eq "Yes"
+      end
+
+      it 'returns No if water is not on' do
+        garden = Garden.create!( name:"North Boulder Community Garden",
+                                        water_on: false,
+                                        water_access_pts: 2,
+                                        created_at: Time.now,
+                                        updated_at: Time.now
+                                      )
+        expect(garden.water_on_text).to eq "No"
+      end
     end
   end
 end
