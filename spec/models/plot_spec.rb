3bc2cdd98b5336e1f739b25245b6adce5d3829b2
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-13 16:00:47 -0600

    Update plot model test with before hook

--- spec/models/plot_spec.rb
+++ spec/models/plot_spec.rb
@@ -6,37 +6,37 @@ RSpec.describe Plot do
   end
 
   describe 'instance methods' do
+    before :each do
+      @garden = Garden.create!( name:"North Boulder Community Garden",
+                                      water_on: false,
+                                      water_access_pts: 2,
+                                      created_at: Time.now,
+                                      updated_at: Time.now
+                                    )
+      @plot1 = @garden.plots.create!(  name: "The Grove",
+                            available: true,
+                            sun_coverage: :full_shade,
+                            square_ft: 250
+                          )
+      @plot2 = @garden.plots.create!(  name: "The Hive",
+                            available: false,
+                            sun_coverage: :full_sun,
+                            square_ft: 300
+                          )
+    end
+
     describe '#available_text' do
       it 'returns Yes if plot is available' do
-        garden = Garden.create!( name:"North Boulder Community Garden",
-                                        water_on: false,
-                                        water_access_pts: 2,
-                                        created_at: Time.now,
-                                        updated_at: Time.now
-                                      )
-        plot = garden.plots.create!(  name: "The Grove",
-                              available: true,
-                              sun_coverage: :full_shade,
-                              square_ft: 250
-                            )
-
-      expect(plot.available_text).to eq "Yes"
+        expect(@plot1.available_text).to eq "Yes"
       end
 
       it 'returns No if plot is not available' do
-        garden = Garden.create!( name:"North Boulder Community Garden",
-                                        water_on: false,
-                                        water_access_pts: 2,
-                                        created_at: Time.now,
-                                        updated_at: Time.now
-                                      )
-        plot = garden.plots.create!(  name: "The Grove",
-                              available: false,
-                              sun_coverage: :full_shade,
-                              square_ft: 250
-                            )
-        expect(plot.available_text).to eq "No"
+        expect(@plot2.available_text).to eq "No"
       end
     end
+
+    # describe '#sun_coverage_text' do
+    #
+    # end
   end
 end
