3ebcf45e4d749bc543da38351e032b90a9b6fb6b
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-14 00:23:08 +0200

    Remove commas from instances

--- spec/features/flower_shops/show_spec.rb
+++ spec/features/flower_shops/show_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe "flower shops show page", type: :feature do
   before :each do
     @mikes_flowers = FlowerShop.create!( name:"Mikes Flower shop",
                                         rating: 3,
-                                        same_day_delivery: false,
+                                        same_day_delivery: false
                                       )
     @floral_designs = FlowerShop.create!( name:"Floral Designs",
                                         rating: 4,
-                                        same_day_delivery: true,
+                                        same_day_delivery: true
                                   )
     @flower_depot = FlowerShop.create!( name:"Flower Depot",
                                         rating: 5,
-                                        same_day_delivery: true,
+                                        same_day_delivery: true
                                   )
   end
 
