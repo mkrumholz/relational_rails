22ab3811ac7c2ede75bc6ad7d3d99d6ea10d6f17
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-13 22:06:58 +0200

    Add feature test for flower shop index

---
+++ spec/features/flower_shops/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "flower shops index page", type: :feature do
+  it "can see the name of all the flower shops" do
+    mikes_flowers = FlowerShop.create!( name:"Mikes Flower shop",
+                                        rating: 3,
+                                        same_day_delivery: false,
+                                      )
+    floral_designs = FlowerShop.create!( name:"Floral Designs",
+                                        rating: 4,
+                                        same_day_delivery: true,
+                                  )
+    flower_depot = FlowerShop.create!( name:"Flower Depot",
+                                        rating: 5,
+                                        same_day_delivery: true,
+                                  )
+    visit "/flower_shops"
+
+    expect(page).to have_content(mikes_flowers.name)
+    expect(page).to have_content(floral_designs.name)
+    expect(page).to have_content(flower_depot.name)
+  end
+end
