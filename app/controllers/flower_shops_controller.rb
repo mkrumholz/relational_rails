b64425b4ff5eedf376dae6db3b5de453a394ade5
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-13 17:12:07 -0600

    Merge main into branch

--- app/controllers/flower_shops_controller.rb
+++ app/controllers/flower_shops_controller.rb
@@ -2,4 +2,8 @@ class FlowerShopsController < ApplicationController
   def index
     @flower_shops = FlowerShop.all
   end
+
+  def show
+    @flower_shops = FlowerShop.find(params[:id])
+  end
 end
