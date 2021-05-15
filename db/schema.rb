74491941e49ecfef0c166d10fb013018a50ed237
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-15 19:09:27 +0200

    Create flowers data base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_220308) do
+ActiveRecord::Schema.define(version: 2021_05_15_155523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2021_05_11_220308) do
     t.string "name"
     t.integer "rating"
     t.boolean "same_day_delivery"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flowers", force: :cascade do |t|
+    t.string "species"
+    t.integer "shelf_life"
+    t.boolean "in_stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
