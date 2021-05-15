cd0e59f23f3d29d9d9d34f2ba79ac19410930501
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-15 10:55:09 -0600

    Create test for garden ordering

--- spec/features/gardens/index_spec.rb
+++ spec/features/gardens/index_spec.rb
@@ -1,27 +1,39 @@
+# User Story 6, Parent Index sorted by Most Recently Created (x2)
+#
+# As a visitor
+# When I visit the parent index,
+# I see that records are ordered by most recently created first
+# And next to each of the records I see when it was created
+
 require 'rails_helper'
 
 RSpec.describe 'gardens index' do
   before :each do
     @north_boulder = Garden.create!( name:"North Boulder Community Garden",
                                     water_on: false,
-                                    water_access_pts: 2
+                                    water_access_pts: 2,
+                                    created_at: Time.parse('2021-02-01')
                                   )
     @south_boulder = Garden.create!( name:"South Boulder Community Garden",
                                     water_on: false,
-                                    water_access_pts: 3
+                                    water_access_pts: 3,
+                                    created_at: Time.parse('2021-04-01')
                                   )
     @tuinpark = Garden.create!( name:"Tuinpark Ons Buiten",
                                     water_on: true,
-                                    water_access_pts: 4
+                                    water_access_pts: 4,
+                                    created_at: Time.parse('2021-01-01')
                                   )
   end
 
-  it 'shows all of the garden names' do
+  it 'shows all of the garden names in order of date created' do
     visit '/gardens'
 
-    expect(page).to have_content(@north_boulder.name)
-    expect(page).to have_content(@south_boulder.name)
-    expect(page).to have_content(@tuinpark.name)
+    # expect(page).to have_content(@north_boulder.name)
+    # expect(page).to have_content(@south_boulder.name)
+    # expect(page).to have_content(@tuinpark.name)
+    expect(@tuinpark.name).to appear_before(@north_boulder.name)
+    expect(@north_boulder.name).to appear_before(@south_boulder.name)
   end
 
   it 'links to each garden show page' do
