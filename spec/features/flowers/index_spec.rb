817f8b0411adf42c796827fe7f62698c33b405dd
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-15 20:14:55 +0200

    Create flower index link to show page

--- spec/features/flowers/index_spec.rb
+++ spec/features/flowers/index_spec.rb
@@ -1,17 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'flowers index page' do
+  before :each do
+    @flower1 = Flower.create!( species:"Rose",
+                                        shelf_life: 3,
+                                        in_stock: false
+                                      )
+    @flower2 = Flower.create!( species:"Sunflower",
+                                        shelf_life: 4,
+                                        in_stock: true
+                                  )
+    @flower3 = Flower.create!( species:"Tulip",
+                                        shelf_life: 5,
+                                        in_stock: true
+                                  )
+  end
   it 'shows all flowers names' do
-    flower1 = Flower.create!(species: "Rose", shelf_life: 5, in_stock: true)
-    flower2 = Flower.create!(species: "Aloe Vera", shelf_life: 6, in_stock: true)
     visit "/flowers"
 
 
-    expect(page).to have_content(flower1.species)
-    expect(page).to have_content(flower1.shelf_life)
-    expect(page).to have_content(flower1.in_stock)
-    expect(page).to have_content(flower2.species)
-    expect(page).to have_content(flower2.shelf_life)
-    expect(page).to have_content(flower2.in_stock)
+    expect(page).to have_content(@flower1.species)
+    expect(page).to have_content("Shelf Life: 3 Days")
+    expect(page).to have_content("In Stock: false")
+  end
+
+  it 'links to each flowers show page' do
+    visit '/flowers'
+
+    click_on 'Rose'
+
+    expect(current_path).to eq("/flowers/#{@flower1.id}")
   end
 end
