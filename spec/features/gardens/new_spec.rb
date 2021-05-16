6ab0778f465dcada50bd99293ab9f3071015d186
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-16 12:29:44 -0600

    Remove comment

--- spec/features/gardens/new_spec.rb
+++ spec/features/gardens/new_spec.rb
@@ -1,14 +1,3 @@
-# As a visitor
-# When I visit the Parent Index page
-# Then I see a link to create a new Parent record, "New Parent"
-# When I click this link
-# Then I am taken to '/parents/new' where I see a form for a new parent record
-# When I fill out the form with a new parent's attributes:
-# And I click the button "Create Parent" to submit the form
-# Then a POST request is sent to the '/parents' route,
-# a new parent record is created,
-# and I am redirected to the Parent Index page where I see the new Parent displayed.
-
 require 'rails_helper'
 
 RSpec.describe 'create garden' do
