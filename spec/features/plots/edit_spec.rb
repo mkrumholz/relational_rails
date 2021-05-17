06519d4846d05baa9bdf40fab4a1dd19d3b14dfd
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-16 22:10:52 -0600

    Create tests for plots#edit functionality

--- spec/features/plots/edit_spec.rb
+++ spec/features/plots/edit_spec.rb
@@ -11,7 +11,30 @@
 require 'rails_helper'
 
 RSpec.describe 'update plot' do
-  it 'can update the name of a plot'
+  before :each do
+    @north_boulder = Garden.create!( name:"North Boulder Community Garden",
+                                      water_on: false,
+                                      water_access_pts: 2
+                                    )
+    @grove         = @north_boulder.plots.create!( name: "The Grove",
+                                                available: true,
+                                                sun_coverage: :full_shade,
+                                                square_ft: 250
+                                              )
+  end
+  it 'can update the name of a plot' do
+    visit "/plots/#{@grove.id}"
+
+    click_button 'Edit'
+
+    expect(current_path).to eq("/plots/#{@grove.id}/edit")
+
+    fill_in 'Name', with: 'The Clover Patch'
+    click_button 'Update'
+
+    expect(current_path).to eq("/plots/#{@grove.id}")
+    expect(page).to have_content('The Clover Patch')
+  end
 
   it 'can update whether a plot is available'
 
