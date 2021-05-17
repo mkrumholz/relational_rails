d81c88ad3fce936d50e1c5f64a314f993182caff
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-16 21:43:48 -0600

    Remove comment

--- spec/features/gardens/plots/new_spec.rb
+++ spec/features/gardens/plots/new_spec.rb
@@ -1,13 +1,3 @@
-# As a visitor
-# When I visit a Parent Childs Index page
-# Then I see a link to add a new adoptable child for that parent "Create Child"
-# When I click the link
-# I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
-# When I fill in the form with the child's attributes:
-# And I click the button "Create Child"
-# Then a POST request is sent to '/parents/:parent_id/child_table_name',
-# a new child object/row is created for that parent,
-# and I am redirected to the Parent Childs Index page where I can see the new child listed
 require 'rails_helper'
 
 RSpec.describe 'create plot for a specific garden' do
