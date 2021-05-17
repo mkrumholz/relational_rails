1d8fdd0a5d78ca6352488281076eba7ffb80d771
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-17 17:06:30 -0600

    Remove comment

--- spec/features/gardens/plots/index_spec.rb
+++ spec/features/gardens/plots/index_spec.rb
@@ -1,11 +1,3 @@
-# User Story 16, Sort Parent's Children in Alphabetical Order by name (x2)
-#
-# As a visitor
-# When I visit the Parent's children Index Page
-# Then I see a link to sort children in alphabetical order
-# When I click on the link
-# I'm taken back to the Parent's children Index Page where I see all of the parent's children in alphabetical order
-
 require 'rails_helper'
 
 RSpec.describe 'garden plots index' do
