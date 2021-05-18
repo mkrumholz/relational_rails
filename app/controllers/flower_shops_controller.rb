5094fbb47408209080038d3ad9a9ce0d6b1aa5cc
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-18 15:23:43 +0200

    Add edit and update actions

--- app/controllers/flower_shops_controller.rb
+++ app/controllers/flower_shops_controller.rb
@@ -15,6 +15,16 @@ class FlowerShopsController < ApplicationController
     redirect_to '/flower_shops'
   end
 
+  def edit
+    @flower_shop = FlowerShop.find(params[:id])
+  end
+
+  def update
+    flower_shop = FlowerShop.find(params[:id])
+    flower_shop.update(flower_shop_params)
+    redirect_to "/flower_shops/#{params[:id]}"
+  end
+
   private
   def flower_shop_params
     params.permit(:name, :rating, :same_day_delivery)
