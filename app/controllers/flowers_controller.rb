fbeb631b777281151007fd10d18791ba7c8d19a6
Jermaine Braumuller <braumuller02@gmail.com>
2021-05-18 21:02:46 +0200

    Create edit actions for flower controller

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -7,6 +7,16 @@ class FlowersController < ApplicationController
     @flower = Flower.find(params[:id])
   end
 
+  def edit
+    @flower = Flower.find(params[:id])
+  end
+
+  def update
+    flower = Flower.find(params[:id])
+    flower.update(flower1_params)
+    redirect_to "/flowers/#{flower.id}"
+  end
+
   private
     def flower_params
       params.permit(:species, :shelf_life, :in_stock)
