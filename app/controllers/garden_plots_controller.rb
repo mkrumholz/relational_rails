0035625555316883a06dfed7f7959c78269ac6df
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-19 12:39:58 -0600

    Refactor garden plots controller

--- app/controllers/garden_plots_controller.rb
+++ app/controllers/garden_plots_controller.rb
@@ -2,10 +2,8 @@ class GardenPlotsController < PlotsController
   def index
     @garden = Garden.find(params[:id])
     @plots = @garden.plots
-    if params[:sort] == 'by_name'
-      @plots = @garden.plots_by_name
-    elsif !params[:square_ft].nil?
-      @plots = @garden.min_square_ft(params[:square_ft])
+    if !params[:sort].nil? || !params[:square_ft].nil?
+      @plots = update_view(@garden, params)
     end
   end
 
@@ -18,4 +16,13 @@ class GardenPlotsController < PlotsController
     plot = garden.plots.create(plot_params)
     redirect_to "/gardens/#{garden.id}/plots"
   end
+
+  private
+    def update_view(garden, params)
+      if params[:sort] == 'by_name'
+        garden.plots_by_name
+      elsif !params[:square_ft].nil?
+        garden.min_square_ft(params[:square_ft])
+      end
+    end
 end
